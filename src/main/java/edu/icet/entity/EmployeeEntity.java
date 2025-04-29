775c5cb7094be326f6c66938198dbcc9fd722399
bavindushan <bavindushan65@gmail.com>
2025-04-29 10:14:50 +0530

    EmployeeEntity class done

---
+++ src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -0,0 +1,34 @@
+package edu.icet.entity;
+
+import edu.icet.utill.DepartmentType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    private DepartmentType department;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
